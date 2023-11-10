a5957664f48a9cb5266b688c1975656f4a0a709d
jordimall <jordi.mallafre@hotmail.com>
2023-11-10 12:34:33 +0100

    Creación e implementación de los service

--- src/main/java/com/example/demo/dto/Proyecto.java
+++ src/main/java/com/example/demo/dto/Proyecto.java
@@ -16,14 +16,13 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 /**
  * 
  */
 @Entity
 @Table(name = "proyectos")
-public @Data class Proyecto {
+public class Proyecto {
 
 	@Id
 	@Column(name = "Id")
@@ -41,4 +40,80 @@ public @Data class Proyecto {
 
 	@JsonIgnoreProperties("proyectos")
 	private List<Cientifico> cientificos = new ArrayList<>();
+
+	/**
+	 * 
+	 */
+	public Proyecto() {
+	}
+
+	/**
+	 * @param id
+	 * @param nombre
+	 * @param horas
+	 * @param cientificos
+	 */
+	public Proyecto(String id, String nombre, int horas, List<Cientifico> cientificos) {
+		this.id = id;
+		this.nombre = nombre;
+		this.horas = horas;
+		this.cientificos = cientificos;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @return the horas
+	 */
+	public int getHoras() {
+		return horas;
+	}
+
+	/**
+	 * @return the cientificos
+	 */
+	public List<Cientifico> getCientificos() {
+		return cientificos;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @param horas the horas to set
+	 */
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
+	/**
+	 * @param cientificos the cientificos to set
+	 */
+	public void setCientificos(List<Cientifico> cientificos) {
+		this.cientificos = cientificos;
+	}
+
 }
